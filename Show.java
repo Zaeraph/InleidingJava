263cbcc5e7c08d652ab76e0c55b30b6a715e5f40
Zaeraph <Out.damien768@gmail.com>
2016-09-28 21:50:40 +0200

    Add files via upload

--- Show.java
+++ Show.java
@@ -2,13 +2,17 @@
 import java.applet.Applet;
 import java.awt.*;
 
- public class Show extends Applet {
+
+public class Show extends Applet {
 
  public void init() {
-  setBackground(Color.blue);
+   setBackground(Color.WHITE);
  }
   public void paint(Graphics g) {
-   g.setColor(Color.yellow);
-   g.drawString("Damien Out", 50, 60);
+   g.setColor(Color.blue);
+   g.drawString("Damien", 50, 60);
+   g.setColor(Color.red );
+           g.drawString("Out", 50, 70);
   }
  }
+
